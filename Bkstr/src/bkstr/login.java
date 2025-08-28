d288a1affcb416d282ad878a032702a2df1d5bb9
HEELJerichohoho <nanalesjeck12@gmail.com>
2025-08-29 01:28:59 +0800

    Update login.java

--- Bkstr/src/bkstr/login.java
+++ Bkstr/src/bkstr/login.java
@@ -7,59 +7,77 @@ public class login extends JFrame {
 	private JTextField txtuser;
 	private JPasswordField txtpass;
 	public login() {
+		setTitle("Welcome to the Bookstore!");
 		getContentPane().setLayout(null);
 		
 		JPanel bgpanel = new JPanel();
-		bgpanel.setBounds(0, 0, 369, 396);
+		bgpanel.setBounds(0, 0, 369, 357);
+		bgpanel.setLayout(null);
 		getContentPane().add(bgpanel);
 		
+		
+		JLabel bg = new JLabel();
+		bg.setBounds(0, 0, 369, 357);
+		
+		ImageIcon bckgrnd = new ImageIcon("C:\\Users\\LENOVO\\Desktop\\Liblib\\bs.jpg");
+		Image img = bckgrnd.getImage();
+		Image scaledImg = img.getScaledInstance(bg.getWidth(), bg.getHeight(), Image.SCALE_SMOOTH);
+		bg.setIcon(new ImageIcon(scaledImg));
+		bgpanel.add(bg);
+		
 		JPanel logpanel = new JPanel();
-		logpanel.setBounds(370, 0, 369, 396);
+		logpanel.setBackground(new Color(139, 69, 19));
+		logpanel.setBounds(370, 0, 352, 357);
 		getContentPane().add(logpanel);
 		logpanel.setLayout(null);
 		
 		txtuser = new JTextField();
-		txtuser.setBounds(110, 132, 192, 29);
+		txtuser.setBounds(110, 109, 192, 29);
 		logpanel.add(txtuser);
 		txtuser.setColumns(10);
 		
 		txtpass = new JPasswordField();
-		txtpass.setBounds(110, 183, 192, 29);
+		txtpass.setBounds(110, 159, 192, 29);
 		logpanel.add(txtpass);
 		
 		JLabel lblforgotpass = new JLabel("Forgot Password?");
-		lblforgotpass.setForeground(new Color(139, 69, 19));
+		lblforgotpass.setForeground(Color.WHITE);
 		lblforgotpass.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblforgotpass.setBounds(160, 212, 105, 23);
+		lblforgotpass.setBounds(158, 184, 105, 23);
+		lblforgotpass.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		logpanel.add(lblforgotpass);
 		
 		JButton btnlogin = new JButton("Login");
+		btnlogin.setForeground(new Color(139, 69, 19));
+		btnlogin.setBackground(Color.WHITE);
 		btnlogin.setFont(new Font("Tahoma", Font.BOLD, 12));
-		btnlogin.setBounds(110, 247, 192, 29);
+		btnlogin.setBounds(110, 211, 192, 29);
+		btnlogin.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		logpanel.add(btnlogin);
 		
 		JLabel lblsignup = new JLabel("Sign Up");
-		lblsignup.setForeground(new Color(139, 69, 19));
+		lblsignup.setForeground(Color.WHITE);
 		lblsignup.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblsignup.setBounds(181, 280, 49, 29);
+		lblsignup.setBounds(183, 237, 49, 29);
+		lblsignup.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		logpanel.add(lblsignup);
 		
 		JLabel lblusername = new JLabel("Username");
 		lblusername.setVerticalAlignment(SwingConstants.TOP);
-		lblusername.setForeground(Color.BLACK);
+		lblusername.setForeground(Color.WHITE);
 		lblusername.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblusername.setBounds(42, 138, 62, 23);
+		lblusername.setBounds(42, 115, 62, 23);
 		logpanel.add(lblusername);
 		
 		JLabel lblpassword = new JLabel("Password");
 		lblpassword.setVerticalAlignment(SwingConstants.TOP);
-		lblpassword.setForeground(Color.BLACK);
+		lblpassword.setForeground(Color.WHITE);
 		lblpassword.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblpassword.setBounds(42, 190, 62, 23);
+		lblpassword.setBounds(42, 165, 62, 23);
 		logpanel.add(lblpassword);
 		
 		
-		setSize(738, 396);
+		setSize(739, 389);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
