e6faff33169c2c2b59277666f42b2ecf50e7be4c
HEELJerichohoho <nanalesjeck12@gmail.com>
2025-08-29 20:34:27 +0800

    Update main.java

--- Bkstr/src/bkstr/main.java
+++ Bkstr/src/bkstr/main.java
@@ -3,6 +3,10 @@ package bkstr;
 public class main {
 
 	public static void main(String[] args) {
+		
+		splash spl = new splash(3000);
+		spl.showsplash(3000);
+		
 		login log = new login();
 		log.setVisible(true);
 	}
